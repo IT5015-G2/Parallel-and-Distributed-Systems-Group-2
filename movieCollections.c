dbb2fbfe78a579f98903a2e9484157748fbe9c4d
Kitt <kittyap12@gmail.com>
2018-06-14 20:12:02 +0800

    Marvel Movies (Added SortByYear)
    Changes Made:
    -Added "int year" in the mov structure
    -Added sortByYear(collect A)
    -Added "printf("\n YEAR: %d",A.movie[x].year);"

--- movieCollections.c
+++ movieCollections.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
 #include<strings.h>
 
 //STRUCTURES
 typedef struct movies{
 	char title[60];
 	int ratings;
+	int year;
 	float income;
 	float budget;
 }mov;
@@ -17,6 +19,7 @@ typedef struct collections{
 //FUNCTIONS
 void initialize(collect *A);
 void display(collect A);
+void sortByYear(collect A);
 
 int main()
 {
@@ -36,6 +39,7 @@ void display(collect A)
 	
 	for(x = 0;x < A.cnt;x++){
 		printf("\n TITLE: %s",A.movie[x].title);
+		printf("\n YEAR: %d",A.movie[x].year);
 		printf("\nRATINGS: %d",A.movie[x].ratings);
 		printf("\nINCOME: %.2f",A.movie[x].income);
 		printf("\nBUDGET: %.2f",A.movie[x].budget);
@@ -47,3 +51,29 @@ void initialize(collect *A)
 	A->cnt = 0;
 	//INSERT CODE FOR AUTO GENERATE DATA INPUTS
 }
+void sortByYear(collect A){
+		int i, j;
+	mov movie;
+	for (i = 0; i < 20; ++i) 
+        {
+ 
+            for (j = i + 1; j < 20; ++j)
+            {
+ 
+                if (A.movie[i].year > A.movie[j].year) 
+                {
+ 
+                    movie =  A.movie[i];
+                    A.movie[i] = A.movie[j];
+                    A.movie[j] = movie;
+ 
+                }
+ 
+            }
+ 
+        }
+        display(A);
+	
+}
+
+
